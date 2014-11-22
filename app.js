823aef0498e594f555549e9559d8ab01c6c04a5e
Clayton Hynfield <clayton@hynfield.org>
2014-11-21 23:38:00 -0500

    Use dot notation

--- app.js
+++ app.js
@@ -27,19 +27,19 @@ function closest(color, palette) {
 }
 
 var L = {
-    'xx-low': 15,
+    'xx_low': 15,
     'low': 25,
     'medium': 50,
     'high': 75,
-    'xx-high': 95
+    'xx_high': 95
 };
 
 var c = {
-    'x-low': 15,
+    'x_low': 15,
     'low': 33,
     'medium': 50,
     'high': 67,
-    'x-high': 85,
+    'x_high': 85,
 };
 
 var h = {
@@ -53,20 +53,20 @@ var h = {
     'magenta': 276 + 60 - 15,  // complement triad
 };
 
-var black = new chroma.lch(15, 15, h['blue']);
-var white = new chroma.lch(95, 15, h['yellow']);
-var orange = new chroma.lch(L['medium'], c['high'], h['orange']);
+var black = new chroma.lch(15, 15, h.blue);
+var white = new chroma.lch(95, 15, h.yellow);
+var orange = new chroma.lch(L.medium, c.high, h.orange);
 var greyscale = chroma.scale([black, white]);
 
-var whiteHighlight = chroma.scale([ white, chroma.lch(L['xx-low'], c['high'], h['orange']) ]);
+var whiteHighlight = chroma.scale([ white, chroma.lch(L.xx_low, c.high, h.orange) ]);
 console.log('hlwhite', whiteHighlight(1).hex());
-var blackHighlight = chroma.scale([ black, chroma.lch(L['xx-high'], c['high'], h['orange']) ]);
+var blackHighlight = chroma.scale([ black, chroma.lch(L.xx_high, c.high, h.orange) ]);
 console.log('hlblack', blackHighlight(1).hex());
 
 Object.keys(h).map(function (color, index) {
     palette.push(new chroma.lch(
-        L['medium'],
-        c['low'],
+        L.medium,
+        c.low,
         h[color]
     ));
 });
